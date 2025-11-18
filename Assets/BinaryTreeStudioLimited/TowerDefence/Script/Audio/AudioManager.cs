1a052ddffe562951135f0b01df82dfcafa447ce9
stephenwong1997 <32104446+stephenwong1997@users.noreply.github.com>
2025-11-18 20:08:06 +0800

    New Attack Audio

--- Assets/BinaryTreeStudioLimited/TowerDefence/Script/Audio/AudioManager.cs
+++ Assets/BinaryTreeStudioLimited/TowerDefence/Script/Audio/AudioManager.cs
@@ -10,13 +10,21 @@ namespace TowerDefence
         [SerializeField] private List<AudioSource> BGMList = new List<AudioSource>();
 
         [Header("Sound Effect")]
+        [Header("System")]
         [SerializeField] private AudioSource gameStart;
         [SerializeField] private AudioSource gameOver;
         [SerializeField] private AudioSource gameWin;
         [SerializeField] private AudioSource normalHit;
         [SerializeField] private AudioSource criticalHit;
         [SerializeField] private AudioSource playerHurt;
+
+        [Header("Attack")]
         [SerializeField] private AudioSource fireBall;
+        [SerializeField] private AudioSource iceBall;
+        [SerializeField] private AudioSource iceArea;
+        [SerializeField] private AudioSource poisonBall;
+        [SerializeField] private AudioSource poisonArea;
+        [SerializeField] private AudioSource rockBall;
 
         #region BGM
 
@@ -32,13 +40,26 @@ namespace TowerDefence
         #endregion
 
         #region SFX
+
+        #region System
         public void PlayGameStartAudio() => gameStart.Play();
         public void PlayGameOverAudio() => gameOver.Play();
         public void PlayGameWinAudio() => gameWin.Play();
         public void PlayNormalHitAudio() => normalHit.Play();
         public void PlayCriticalHitAudio() => criticalHit.Play();
         public void PlayPlayerHurtAudio() => playerHurt.Play();
+
+        #endregion
+
+        #region Attack
         public void PlayFireBallAudio() => fireBall.Play();
+        public void PlayIceBallAudio() => iceBall.Play();
+        public void PlayIceAreaAudio() => iceArea.Play();
+        public void PlayPoisonBallAudio() => poisonBall.Play();
+        public void PlayPoisonAreaAudio() => poisonArea.Play();
+        public void PlayRockBallAudio() => rockBall.Play();
+
+        #endregion
 
         #endregion
 
